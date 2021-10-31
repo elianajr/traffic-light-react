1e6be446b88eda55f17373c83ab417ed8ed53429
elianajr <elianajr1@gmail.com>
2021-10-31 15:07:40 +0000

    adding css

--- src/js/component/bulb.jsx
+++ src/js/component/bulb.jsx
@@ -2,32 +2,18 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const Bulb = props => {
-	// const [color, setColor] = useState(props.color);
-	// const [on, setOn] = useState(props.light);
-
 	return (
 		<div
 			className={"bulb ".concat(props.color)}
 			onClick={() => {
-				props.switchBulb(props.color), console.log(props.color);
+				props.switchBulb(props.color);
 			}}></div>
-		// <div
-		// 	className={`bulb ${color}`}
-		// 	onClick={() => {
-		// 		if (color == "") {
-		// 			props.switchBulb(props.color);
-
-		// 		} else {
-		// 			setColor("");
-		// 		}
-		// 	}}></div>
 	);
 };
 
 Bulb.propTypes = {
 	color: PropTypes.string,
 	switchBulb: PropTypes.func
-	// light: PropTypes.bool
 };
 
 export default Bulb;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,71 +8,47 @@ const Home = () => {
 		go: "green"
 	});
 
-	// const [lightFirst, setLightFirst] = useState("");
-	// const [lightSecond, setLightSecond] = useState("");
-	// const [lightThird, setLightThird] = useState("");
-
-	// const switchBulb = color => {
-	// 	if (color == "stop") {
-	// 		setLightFirst(true);
-	// 		setLightSecond(false);
-	// 		setLightThird(false);
-	// 	} else if (color == "slow") {
-	// 		setLightFirst(false);
-	// 		setLightSecond(true);
-	// 		setLightThird(false);
-	// 	} else if (color == "go") {
-	// 		setLightFirst(false);
-	// 		setLightSecond(false);
-	// 		setLightThird(false);
-	// 	}
-	// };
-
 	return (
-		<div className="traffic-light">
-			<Bulb
-				className="red glow"
-				key="stop"
-				color={color.stop}
-				switchBulb={() => {
-					setColor({
-						stop: "red glow",
-						slow: "",
-						go: ""
-					});
-				}}
-				// light={lightFirst}
-				// switchLight={switchBulb}
-			/>
-			<Bulb
-				className="yellow glow"
-				key="slow"
-				color={color.slow}
-				switchBulb={() => {
-					setColor({
-						stop: "",
-						slow: "yellow glow",
-						go: ""
-					});
-				}}
-				// light={lightSecond}
-				// switchLight={switchBulb}
-			/>
-			<Bulb
-				className="green glow"
-				key="go"
-				color={color.go}
-				switchBulb={() => {
-					setColor({
-						stop: "",
-						slow: "",
-						go: "green glow"
-					});
-				}}
-				// color="go"
-				// light={lightThird}
-				// switchLight={switchBulb}
-			/>
+		<div>
+			<div className="traffic-light">
+				<Bulb
+					// className="red glow"
+					key="stop"
+					color={color.stop}
+					switchBulb={() => {
+						setColor({
+							stop: "red glow",
+							slow: "",
+							go: ""
+						});
+					}}
+				/>
+				<Bulb
+					// className="yellow glow"
+					key="slow"
+					color={color.slow}
+					switchBulb={() => {
+						setColor({
+							stop: "",
+							slow: "yellow glow",
+							go: ""
+						});
+					}}
+				/>
+				<Bulb
+					// className="green glow"
+					key="go"
+					color={color.go}
+					switchBulb={() => {
+						setColor({
+							stop: "",
+							slow: "",
+							go: "green glow"
+						});
+					}}
+				/>
+			</div>
+			<div className="base"></div>
 		</div>
 	);
 };
